6fb50d5b278d9a163ebb5790be4187fd7791903a
jefersongjr <jeferson_gjr@hotmail.com>
2022-08-31 14:23:44 -0300

    feat: requisito 1 - criando o endpoint get /talker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkerFile = require('./readAndWriteFiles');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +12,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (request, response) => {
+  const talker = await talkerFile.getAllTalkers();
+  response.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
